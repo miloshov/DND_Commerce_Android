e94a337d6fe86986b9fe2e4f8a6cf72680ec5bbf
arsayu <nikolayu@gmail.com>
2017-09-05 04:05:38 +0200

    Added design

--- app/src/main/java/com/hajora/dndcommerce/Ask.java
+++ app/src/main/java/com/hajora/dndcommerce/Ask.java
@@ -1,7 +1,12 @@
 package com.hajora.dndcommerce;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * Created by nikol on 05-Sep-17.
@@ -12,6 +17,19 @@ public class Ask extends Activity {
         super.onCreate(savedInstanceState);
         // Get the view from contact.xml
         setContentView(R.layout.ask);
-
+    Button send = (Button) findViewById(R.id.send_ask);
+    send.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            TextView name = (TextView) findViewById(R.id.name_ask);
+            TextView email = (TextView) findViewById(R.id.email_ask);
+            TextView text = (TextView) findViewById(R.id.text_ask);
+            Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto", email.getText().toString() , null));
+            emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Kontakt:"+name.getText().toString());
+            emailIntent.putExtra(Intent.EXTRA_TEXT, text.getText().toString() );
+            startActivity(Intent.createChooser(emailIntent, "Pošalji..."));
+        }
+    });
     }
+
 }
